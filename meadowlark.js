7f640c578781eb365dd4f84225da2bef4a4c028c
Mars <851104757@qq.com>
2016-03-17 18:12:54 +0800

    QA测试框架

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+//访问外部数组
+var fortune = require('./lib/fortune.js');
+
+/*
+//访问内部数组
 var fortunes = [
 	"Conquer your fear or they will conquer you.",
 	"Rivers need springs.",
@@ -8,6 +13,7 @@ var fortunes = [
 	"You will have a pleasant surprise.",
 	"Whenever possible, keep it simple.",
 ]
+*/
 
 //handlebars代码
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
@@ -15,8 +21,15 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+app.use(function(req, res, next){
+	  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	//res.locals.showTests = true;
+	next();
+});
 
 app.use(express.static(__dirname + '/public'));
+
+
 
 app.get('/', function(req, res){
 	/*
@@ -39,8 +52,16 @@ app.get('/', function(req, res){
 
 //随机模式
 app.get('/about', function(req, res){
+	/*
+	//访问内部数组
 	var randomFortune = fortunes[Math.floor(Math.random()* fortunes.length)];
 	res.render('about', {fortune: randomFortune});
+	*/
+	//访问外部数组
+	//res.render('about', {fortune: fortune.getFortune()});
+	
+    //访问外部数组 - QA测试
+	res.render('about', {fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js'});
 });
 
 
---
+++ public/qa/tests-global.js
@@ -0,0 +1,13 @@
+/*
+suite('Global Tests', function(){
+	test('page has a valid title', function(){
+		assert( document.title && document.title.match(/\S/)&& document.title.toUpperCase() !== 'TODO');
+	});
+});
+*/
+suite('Global Tests', function(){
+test('page has a valid title', function(){
+assert(document.title && document.title.match(/\S/) &&
+document.title.toUpperCase() !== 'TODO');
+});
+});
